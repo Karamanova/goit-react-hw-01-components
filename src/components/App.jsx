fbccfabbe42e01ea24885778982353cb155a07f8
Maryna Karamanova <karamanova91@gmail.com>
2023-02-20 01:15:50 +0200

    deleted globalstyle in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import data from 'data/data.json';
 import user from 'data/user.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
-import { GlobalStyle } from './App.styled';
 
 
 export const App = () => {
